b039036a47aaf44cc6909338fba2bcafc79590d3
Json Estupinan <jeison.code@hotmail.com>
2025-11-16 01:00:03 -0500

    Update: App router's responsability

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,12 +1,6 @@
 from fastapi import APIRouter, Request
-from controller.telegram_controller import webhook_handler
+from app.controller.telegram_controller import webhook_handler, router as telegram_router
 
-router = APIRouter()
+router = APIRouter(prefix="/api")
 
-@router.post("/webhook")
-async def webhook(request: Request):
-    try:
-        await webhook_handler(request)
-    except Exception as e:
-        print(f"Error handling webhook: {e}")
-    return {"status": "ok"}
+router.include_router(telegram_router, tags=["TelegramBot"])
